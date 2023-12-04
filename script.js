b1d57d32f4c81bca209d7baab28720ebd91cc4a0
zallen16 <126983111+zallen16@users.noreply.github.com>
2023-12-03 22:44:29 -0500

    filled out all days to appear in schedule

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+
+dayjs().format('hh:mm:ss a');
+$('#currentDay').text(dayjs());
+$(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -14,6 +18,9 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  var currentTime = dayjs().hour();
+  console.log(currentTime)
+  // $('#hour-10').text(dayjs());
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
